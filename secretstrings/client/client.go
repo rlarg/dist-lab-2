9a0a7987c5d5d60aecaa0e455b57b7837f04169b
rlarg <pharadonlarg@gmail.com>
2022-10-18 16:49:24 +0100

    part 3: read words from the wordlist

--- secretstrings/client/client.go
+++ secretstrings/client/client.go
@@ -1,23 +1,39 @@
 package main
 
 import (
+	"bufio"
 	"flag"
 	"fmt"
 	"net/rpc"
+	"os"
 	"uk.ac.bris.cs/distributed1/chat/stubs"
 )
+
+func check(e error) {
+	if e != nil {
+		fmt.Println("Error: exiting")
+		return
+	}
+}
 
 func main() {
 	server := flag.String("server", "127.0.0.1:8030", "IP:port string to connect as server")
 	flag.Parse()
-	fmt.Println("Server:", server)
+	fmt.Println("Server: ", server)
 	client, _ := rpc.Dial("tcp", *server)
 	defer client.Close()
 
-	request := stubs.Request{Message: "Hello"}
+	//request := stubs.Request{Message: "Hello"}
 	response := new(stubs.Response)
-	client.Call(stubs.PremiumReverseHandler, request, response)
-	fmt.Println("Responded:" + response.Message)
+	file, err := os.Open("wordlist")
+	check(err)
+	rd := bufio.NewReader(file)
+	for {
+		line, _ := rd.ReadString('\n')
+		request := stubs.Request{Message: line}
+		client.Call(stubs.PremiumReverseHandler, request, response)
+		fmt.Println("Responded: " + response.Message)
+	}
+	file.Close()
 	// Connect to the RPC server and send the request(s)
-
 }
